853a04dfee807de05254c9162b80a5461c714636
vafah1 <vafah1987@gmail.com>
2016-09-13 16:41:24 -0400

    challenge2commit

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,26 @@
 $(function(){
 
+$('h1').on('click', function(){
+        if($(this).attr('data-click-state') == 1) {
+            $(this).attr('data-click-state', 0)
+            $(this).css('color', 'black')
+        } else {
+            $(this).attr('data-click-state', 1)
+            $(this).css('color', 'red')
+        }
+    });
+
+$("h5:odd").hide();
+
+
+
+$('p').not("aside p").text(function(i,val){
+    	return val.replace("Bacon", "LASER VISION");
+	});
+
+$('.post').slice(-2).hide();
+$("aside, aside *").hide();
+
 	//all your code should go here
 
 })
